f8d14119671167b259bfd19b883f80526e5e2f08
davi dias <davifernandosousadias18@gmail.com>
2024-09-02 17:36:50 -0300

    feat: ex03 resolvido

---
+++ ex03.java
@@ -0,0 +1,4 @@
+public class ex03 {
+    // int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+    // como iteração while tem condição K < "MENOR" 12 "INDICE", TERIA REPETIÇÃO 11 VEZES FAZENDO A (SOMA = A PROPRIA SOMA + K QUE O (K = PROPRIO K + 1)) * 11
+}
