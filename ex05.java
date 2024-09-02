2f89c8a5603f36224ec3ce93c7c22493881e2cce
davi dias <davifernandosousadias18@gmail.com>
2024-09-02 17:37:07 -0300

    feat: ex05 resolvido

---
+++ ex05.java
@@ -0,0 +1,33 @@
+public class ex05 {
+ /*
+ 
+ 
+
+PASSO 1. Ligue o Interruptor A:
+   - Deixe o Interruptor A ligado por 10 a 15 minutos. Isso permitirá que a lâmpada conectada a este interruptor esquente.
+
+PASSO 2. Desligue o Interruptor A e Ligue o Interruptor B:
+   - Após 10 a 15 minutos, desligue o Interruptor A.
+   - Ligue o Interruptor B e deixe-o ligado.
+
+PASSO 3. Vá até as Salas das Lâmpadas:
+   - Lâmpada 1:
+     - Se a lâmpada estiver acesa, ela é controlada pelo Interruptor B (o último que você ligou).
+   - Lâmpada 2:
+     - Se a lâmpada estiver apagada, mas quente ao toque, ela é controlada pelo Interruptor A (o que estava ligado por 10 a 15 minutos e depois desligado).
+   - Lâmpada 3:
+     - Se a lâmpada estiver apagada e fria, ela é controlada pelo Interruptor C (o que nunca foi ligado).
+
+Resumo:
+- Lâmpada Acesa:** Controlada pelo Interruptor B.
+- Lâmpada Apagada e Quente:** Controlada pelo Interruptor A.
+- Lâmpada Apagada e Fria:** Controlada pelo Interruptor C.
+
+
+
+ 
+ 
+ 
+ 
+ */   
+}
