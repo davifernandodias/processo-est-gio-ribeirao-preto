fed2b1fe18f18caafa81b2e6fb70825b3df82e31
davi dias <davifernandosousadias18@gmail.com>
2024-09-02 17:37:01 -0300

    feat: ex04 resolvido

---
+++ ex04.java
@@ -0,0 +1,12 @@
+public class ex04 {
+    /*
+    a) 1, 3, 5, 7, 9  / diferença entre termos consecutivos é 2.
+    b) 2, 4, 8, 16, 32, 64, 128 / sequência onde cada termo é o dobro do anterior.
+    c) 0, 1, 4, 9, 16, 25, 36, 49 /  próximo número é o quadrado da sequencia 0^0,1^1,2^2...
+    d) 4, 16, 36, 64,100 /  quadrados dos números pares
+    e) 1, 1, 2, 3, 5, 8, 13 / sequência fibonacci 
+    f) 2,10, 12, 16, 17, 18, 19,20 / sequência crescente contínua
+    
+    
+    */
+}
